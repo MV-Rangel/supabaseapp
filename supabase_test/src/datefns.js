02ce2385ccc2c1e77bbc56b89dbf12bbd5aad506
Marcus <mrangelpedagogo@gmail.com>
2024-12-16 08:38:25 -0300

    Changes

--- supabase_test/src/datefns.js
+++
@@ -1,9 +0,0 @@
-import { format, compareAsc } from "date-fns";
-import { formatDistance, subDays } from "date-fns";
-
-const x = formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true });
-//=> "3 days ago"
-
-const now = new Date()
-const formatedDate = format(now,`dd MM yy` )
-console.log(formatedDate)
